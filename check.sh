0d023fae384bbee6bcf227b1da2410b5364b3024
NFS Projects <68986092+AnGgIt86@users.noreply.github.com>
2021-06-29 06:24:10 +0700

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,3 +1,3 @@
 echo ubuntuSSH are xyzprjkt property
 git clone https://github.com/kdrag0n/proton-clang proton --depth=1
-git clone https://github.com/xyz-prjkt/xKernelCompiler compiler
+git clone https://github.com/AnGgIt86/xKernelCompiler compiler
