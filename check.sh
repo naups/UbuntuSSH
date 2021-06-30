914ab626cf6663e0c089c4db0d79ca7e1fde0b9b
NFS Projects <68986092+AnGgIt86@users.noreply.github.com>
2021-06-30 14:24:41 +0700

    Add GCC toolchain

--- check.sh
+++ check.sh
@@ -1,3 +1,4 @@
 echo ubuntuSSH are xyzprjkt property
 git clone https://github.com/xyz-prjkt/xRageChain-tc_build proton --depth=1
 git clone https://github.com/AnGgIt86/xKernelCompiler compiler
+git clone https://github.com/cbendot/aarch64-linux-gnu GCC
