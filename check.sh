9cf421651b2c2373ee4022ed2293cea6701cab1a
NFS Projects <68986092+AnGgIt86@users.noreply.github.com>
2021-06-30 11:21:12 +0700

    Change tc build using xRageChain clang 13

--- check.sh
+++ check.sh
@@ -1,3 +1,3 @@
 echo ubuntuSSH are xyzprjkt property
-git clone https://github.com/kdrag0n/proton-clang proton --depth=1
+git clone https://github.com/xyz-prjkt/xRageChain-tc_build proton --depth=1
 git clone https://github.com/AnGgIt86/xKernelCompiler compiler
