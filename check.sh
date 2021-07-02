e8b4d6c2697d3681d07765bf8c9810e43b417011
NFS Projects <68986092+AnGgIt86@users.noreply.github.com>
2021-07-02 15:42:55 +0700

    Update toolchain build using proton clang or xRageChain clang 13

--- check.sh
+++ check.sh
@@ -1,4 +1,5 @@
 echo ubuntuSSH are xyzprjkt property
-git clone https://github.com/xyz-prjkt/xRageChain-tc_build proton --depth=1
+git clone https://github.com/xyz-prjkt/xRageChain-tc_build xRageChain --depth=1
 git clone https://github.com/AnGgIt86/xKernelCompiler compiler
 git clone https://github.com/mvaisakh/gcc-arm64 GCC --depth=1
+git clone https://github.com/kdrag0n/proton-clang proton --depth=1
