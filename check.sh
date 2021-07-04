44f3497bbbd6bf3e01fe4e4d64840ff7f0d8e4d7
NFS Projects <68986092+AnGgIt86@users.noreply.github.com>
2021-07-05 03:29:21 +0700

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
-echo ubuntuSSH are xyzprjkt property
+echo ubuntuSSH are nfs-projects property
 git clone https://github.com/xyz-prjkt/xRageChain-tc_build xRageChain --depth=1
 git clone https://github.com/AnGgIt86/xKernelCompiler compiler
 git clone https://github.com/mvaisakh/gcc-arm64 GCC --depth=1
