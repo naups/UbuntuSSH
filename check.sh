10d72491cbf3f681a2c37f605d5ca732c6438ea3
NFS Projects <68986092+AnGgIt86@users.noreply.github.com>
2021-07-25 05:50:34 +0700

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,1 +1,7 @@
-echo ubuntuSSH are nfs-projects property
+#!/usr/bin/env bash
+
+sudo apt install git aria2 -y
+git clone --depth=1 https://github.com/AnGgIt86/Orangefox_script scripts
+cd scripts
+sudo bash setup/android_build_env.sh
+sudo bash setup/install_android_sdk.sh
