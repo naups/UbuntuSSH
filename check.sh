fefeb8d866c9af101fb6db50b8b5274a2511c592
NFS Projects <68986092+AnGgIt86@users.noreply.github.com>
2021-07-25 06:02:25 +0700

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt install git aria2 -y
-git clone --depth=1 https://github.com/AnGgIt86/Orangefox_script scripts
-cd scripts
-sudo bash setup/android_build_env.sh
-sudo bash setup/install_android_sdk.sh
+sudo apt-get -y update && sudo apt-get -y upgrade
